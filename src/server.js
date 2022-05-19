10526627d84e3933f76694949961b14083b42193
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-05-19 21:40:31 +0900

    middleware 개념 추가 및 테스트

--- src/server.js
+++ src/server.js
@@ -5,10 +5,25 @@ const PORT = 4000;
 // express function을 사용하여 express application 만들기
 const app = express();
 
+// middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed 나.가.</h1>");
+    }
+    console.log("OK. Go GO Go");
+    next();
+};
+
 // route 설정하기
 // root 페이지(/) get request 처리, route handler argument = (requset, response)
 const handleHome = (req, res) => {
-    return res.send({ msg: "I want to you!" });
+    return res.send({ msg: "I want to you!~!" });
 };
 
 // login 페이지 route
@@ -16,8 +31,14 @@ const handleLogin = (req, res) => {
     return res.send("<h1>This is Login Page baby~</h1>");
 };
 
+const handleProtected = (req, res) => {
+    return res.send("안 보일껄ㅋㅋㅋ");
+};
+
+app.use(logger, privateMiddleware);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 // handleListening 메서드
 const handleListening = () =>
