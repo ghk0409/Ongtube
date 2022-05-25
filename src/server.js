8201c7fe7c4f56e83c67c9dda1b5e7136e13c27f
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-05-25 21:51:20 +0900

    router 추가 및 코드정리

--- src/server.js
+++ src/server.js
@@ -1,45 +1,23 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 // express function을 사용하여 express application 만들기
 const app = express();
-
-// middleware
-// const logger = (req, res, next) => {
-//     console.log(`${req.method} ${req.url}`);
-//     next();
-// };
+// morgan의 [dev] 옵션을 통한 logger 설정
 const logger = morgan("dev");
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>Not Allowed 나.가.</h1>");
-    }
-    console.log("OK. Go GO Go");
-    next();
-};
-
-// route 설정하기
-// root 페이지(/) get request 처리, route handler argument = (requset, response)
-const handleHome = (req, res) => res.send({ msg: "I want to you!~!" });
-
-// login 페이지 route
-const handleLogin = (req, res) => {
-    return res.send("<h1>This is Login Page baby~</h1>");
-};
-
-const handleProtected = (req, res) => {
-    return res.send("안 보일껄ㅋㅋㅋ");
-};
-
 // app.use()는 어떤 URL 호출 시에도 동일하게 작동할 middleware 설정할 수 있음
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
+
+// 라우터 설정
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 // handleListening 메서드
 const handleListening = () =>
