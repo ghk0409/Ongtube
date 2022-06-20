30724e8dccec4b10c0b57e76daf641b1dd236c55
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-06-20 22:41:22 +0900

    iteration 및 Mixin 설정
    Pug에서 반복문 설정하기
    mixin을 이용한 HTML block을 함수화하여 분리시키기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,40 @@
-const fakeUser = {
-    username: "Ongs",
-    loggedIn: false,
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "고담시티",
+            rating: 5,
+            comments: 3,
+            createdAt: "2 minutes ago",
+            views: 49,
+            id: 1,
+        },
+        {
+            title: "범죄도시2",
+            rating: 5,
+            comments: 23,
+            createdAt: "5 minutes ago",
+            views: 128,
+            id: 2,
+        },
+        {
+            title: "쥬라기월드 에볼루션",
+            rating: 4,
+            comments: 82,
+            createdAt: "12 minutes ago",
+            views: 316,
+            id: 3,
+        },
+        {
+            title: "신세계백화점",
+            rating: 3,
+            comments: 53,
+            createdAt: "19 minutes ago",
+            views: 149,
+            id: 4,
+        },
+    ];
+    res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-    res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 
