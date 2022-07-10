d1f9951bd40cdc1d98a2b7b6dca84d9131462f52
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-07-10 22:15:41 +0900

    Login 구현
    - getLogin / postLogin 구현
    - 로그인 시, DB 조회해서 Username 확인
    - bcrypt를 이용한 패스워드 비교

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 // 회원가입 GET 컨트롤러 (회원가입 페이지 렌더링)
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -27,21 +28,54 @@ export const postJoin = async (req, res) => {
         });
     }
 
-    await User.create({
-        name,
-        email,
-        username,
-        password,
-        location,
-    });
-    return res.redirect("/login");
+    try {
+        await User.create({
+            name,
+            email,
+            username,
+            password,
+            location,
+        });
+        return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("join", {
+            pageTitle: "Join",
+            errorMessage: error._message,
+        });
+    }
+};
+
+// 로그인 GET 컨트롤러 (로그인 페이지 렌더링)
+export const getLogin = (req, res) =>
+    res.render("login", { pageTitle: "Login" });
+
+// 로그인 POST 컨트롤러 (유저 로그인))
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const pageTitle = "Login";
+    // check if account exists
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(400).render("login", {
+            pageTitle: pageTitle,
+            errorMessage: "An account with this username does not exists.",
+        });
+    }
+    // check if password correct (해싱된 패스워드와 비교!)
+    const loginOk = await bcrypt.compare(password, user.password);
+    if (!loginOk) {
+        return res.status(400).render("login", {
+            pageTitle: pageTitle,
+            errorMessage: "Wrong Password!!",
+        });
+    }
+    console.log("Login Success!!");
+    return res.redirect("/");
 };
 
 export const edit = (req, res) => res.send("Edit users");
 
 export const remove = (req, res) => res.send("remove user!");
-
-export const login = (req, res) => res.send("Login user");
 
 export const logout = (req, res) => res.send("Logout!!");
 
