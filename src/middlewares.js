32299a67fcd7405ec7b81d91305891b5ba46e4df
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-07-18 21:38:52 +0900

    유저프로필 수정 및 프로텍터 구현
    - 유저 프로필 라우터, 컨트롤러, 뷰 구현
    - 로그인, 미로그인 각 상태에 대한 접근을 막기 위해 각각에 대해 protect middleware 구현

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,29 @@
+// session 정보 또는 기본 정보를 res.locals에 저장하기 위한 미들웨어
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "Ongtube";
-    res.locals.loggedInUser = req.session.user;
+    res.locals.loggedInUser = req.session.user || {};
     next();
+};
+
+// 로그인 필요한 웹페이지 접근 보호 미들웨어
+export const protectorMiddleware = (req, res, next) => {
+    // 로그인 상태일 경우 요청을 계속함
+    if (req.session.loggedIn) {
+        return next();
+        // 로그인 상태가 아닐 경우, 로그인 페이지로 리다이렉트
+    } else {
+        return res.redirect("/login");
+    }
+};
+
+// 이미 로그인된 유저가 로그인 미상태용 페이지로 접근을 막기 위한 미들웨어
+export const publicOnlyMiddleware = (req, res, next) => {
+    // 로그인 상태가 아닐 경우, 요청을 계속함
+    if (!req.session.loggedIn) {
+        return next();
+        // 로그인 상태일 경우, 홈으로 리다이렉트
+    } else {
+        return res.redirect("/");
+    }
 };
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -6,14 +6,19 @@ import {
     postLogin,
 } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 // 메인 페이지(root) 라우터
 rootRouter.get("/", home);
 // 회원가입 페이지 라우터 (GET / POST 분리)
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+    .route("/login")
+    .all(publicOnlyMiddleware)
+    .get(getLogin)
+    .post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
